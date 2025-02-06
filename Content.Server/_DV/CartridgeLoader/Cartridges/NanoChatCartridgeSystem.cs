99231cd3ef78c730f71ae804fc0cb3c59818958b
Tobias Berger <toby@tobot.dev>
2025-02-06 14:53:38 +0100

    Nanochat lookup (#2794) redo

--- Content.Server/_DV/CartridgeLoader/Cartridges/NanoChatCartridgeSystem.cs
+++ Content.Server/_DV/CartridgeLoader/Cartridges/NanoChatCartridgeSystem.cs
@@ -596,5 +596,15 @@ public sealed class NanoChatCartridgeSystem : EntitySystem
             notificationsMuted = card.NotificationsMuted;
             listNumber = card.ListNumber;
         }
+
+        var state = new NanoChatUiState(recipients,
+            messages,
+            contacts,
+            currentChat,
+            ownNumber,
+            maxRecipients,
+            notificationsMuted,
+            listNumber);
+        _cartridge.UpdateCartridgeUiState(loader, state);
     }
 }
