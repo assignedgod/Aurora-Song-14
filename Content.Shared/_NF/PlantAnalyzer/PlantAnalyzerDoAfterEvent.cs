0c8bc781cc98ccc11ac3569fbec14a8f88453af7
Timfa <timfalken@hotmail.com>
2025-03-01 18:15:38 +0100

    Port of den's plant analyzer port

--- Content.Shared/_NF/PlantAnalyzer/PlantAnalyzerDoAfterEvent.cs
+++ Content.Shared/_NF/PlantAnalyzer/PlantAnalyzerDoAfterEvent.cs
@@ -1,9 +1,10 @@
-using Content.Shared.DoAfter;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared._NF.PlantAnalyzer;
-
-[Serializable, NetSerializable]
-public sealed partial class PlantAnalyzerDoAfterEvent : SimpleDoAfterEvent
-{
-}
+//using Content.Shared.DoAfter;
+//using Robust.Shared.Serialization;
+//
+//namespace Content.Shared._NF.PlantAnalyzer;
+//
+//[Serializable, NetSerializable]
+//public sealed partial class PlantAnalyzerDoAfterEvent : SimpleDoAfterEvent
+//{
+//}
+//
--- Content.Shared/_NF/PlantAnalyzer/PlantAnalyzerUiKey.cs
+++ Content.Shared/_NF/PlantAnalyzer/PlantAnalyzerUiKey.cs
@@ -1,9 +1,10 @@
-using Robust.Shared.Serialization;
-
-namespace Content.Shared._NF.PlantAnalyzer;
-
-[Serializable, NetSerializable]
-public enum PlantAnalyzerUiKey : byte
-{
-    Key
-}
+//using Robust.Shared.Serialization;
+//
+//namespace Content.Shared._NF.PlantAnalyzer;
+//
+//[Serializable, NetSerializable]
+//public enum PlantAnalyzerUiKey : byte
+//{
+//    Key
+//}
+//
