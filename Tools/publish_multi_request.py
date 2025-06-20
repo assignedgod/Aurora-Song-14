764c0c6d0d21429b7fb12fd9fd90e79e693353b6
GreyScaleHarlequin <195893253+GreyScaleHarlequin@users.noreply.github.com>
2025-06-20 17:28:53 +0100

    Update ROBUST_CDN_URL to new endpoint
    Changed the ROBUST_CDN_URL in publish_multi_request.py from a domain-based URL to a direct IP address with port.

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.aurorasong.com/"
+ROBUST_CDN_URL = "147.124.195.142:27690"
 FORK_ID = "AuroraSong14"
 
 def main():
