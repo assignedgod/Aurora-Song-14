c291cb8821133905b7a80e27528e8ef86d42bbb3
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-08-16 04:08:22 -0300

    Fix Not Being Able to Say No (#1462)
    if something had a default value of on, you couldn't turn it to off
    lmfao
    one of these days im going to make this use booleans instead
    
    :cl:
    - fix: You can now say no to paradox anomalies.

--- Content.Shared/_Floof/Consent/PlayerConsentSettings.cs
+++ Content.Shared/_Floof/Consent/PlayerConsentSettings.cs
@@ -17,7 +17,7 @@ public sealed class PlayerConsentSettings
     public PlayerConsentSettings()
     {
         Freetext = string.Empty;
-        Toggles = new Dictionary<ProtoId<ConsentTogglePrototype>, string>();
+        Toggles = new();
     }
 
     public PlayerConsentSettings(
@@ -28,16 +28,42 @@ public sealed class PlayerConsentSettings
         Toggles = toggles;
     }
 
-    public void EnsureValid(IConfigurationManager configManager, IPrototypeManager prototypeManager)
+    public void EnsureValid(IConfigurationManager configManager,
+        IPrototypeManager prototypeManager,
+        HashSet<ConsentTogglePrototype> togglesPrototypes)
     {
         var maxLength = configManager.GetCVar(CCVars.ConsentFreetextMaxLength); // Flooftier
         Freetext = Freetext.Trim();
         if (Freetext.Length > maxLength)
             Freetext = Freetext.Substring(0, maxLength);
 
-        Toggles = Toggles.Where(t =>
-            prototypeManager.HasIndex<ConsentTogglePrototype>(t.Key)
-            && t.Value == "on"
-        ).ToDictionary();
+        Toggles = GetValidToggles(Toggles, prototypeManager, togglesPrototypes);
+    }
+
+    private Dictionary<ProtoId<ConsentTogglePrototype>, string> GetValidToggles(
+    Dictionary<ProtoId<ConsentTogglePrototype>, string> toggles,
+        IPrototypeManager prototypeManager,
+        HashSet<ConsentTogglePrototype> togglesPrototypes
+    )
+    {
+        var result = new Dictionary<ProtoId<ConsentTogglePrototype>, string>();
+
+        foreach (var toggle in toggles)
+        {
+            var proto = togglesPrototypes
+                .FirstOrDefault(proto => proto.ID == toggle.Key);
+
+            if (proto == null)
+                continue;
+
+            var defaultValue = proto.DefaultValue ? "on" : "off";
+
+            if (defaultValue == toggle.Value)
+                continue;
+
+            result.Add(proto, toggle.Value);
+        }
+
+        return result;
     }
 }
