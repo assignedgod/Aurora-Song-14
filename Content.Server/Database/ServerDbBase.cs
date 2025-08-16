sleepyyapril/TheDen

Clientside Consent (#1306)
This is ONLY to make consent be checkable on client. This does mean that
bad actors _could_ see what you have it set to, but I personally believe
that's a non-issue.

:cl:
- fix: Fixed dragon not being able to chomp anyone.

---------

Co-authored-by: TheDen-Bot <186991657+TheDen-Bot@users.noreply.github.com>

Content.Server/Database/ServerDbBase.cs
@@ -48,9 +48,9 @@ using Content.Server._CD.Records;
 using Content.Server.Administration.Logs;
 using Content.Server.Administration.Managers;
 using Content.Shared._CD.Records;
+using Content.Shared._Floof.Consent;
 using Content.Shared.Administration.Logs;
 using Content.Shared.Clothing.Loadouts.Systems;
-using Content.Shared.Consent;
 using Content.Shared.Database;
 using Content.Shared.Humanoid;
 using Content.Shared.Humanoid.Markings;

assignedgod/Aurora-Song-14

Content.Server/Database/ServerDbBase.cs
@@ -9,8 +9,11 @@ using System.Threading.Tasks;
 using Content.Server.Administration.Logs;
 using Content.Server.Administration.Managers;
 using Content.Shared.Administration.Logs;
-using Content.Shared.Consent; //Floofstation
 using Content.Shared.Construction.Prototypes;
+using Content.Shared._CD.Records;
+using Content.Shared._Floof.Consent;  //Floofstation
+using Content.Shared.Administration.Logs;
+using Content.Shared.Clothing.Loadouts.Systems;
 using Content.Shared.Database;
 using Content.Shared.Ghost.Roles;
 using Content.Shared.Humanoid;