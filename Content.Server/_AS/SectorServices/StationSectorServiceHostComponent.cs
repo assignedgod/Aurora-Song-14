ed86d868937b5296c20d877909a272b6e8b11ec7
AssignedGod <79281911+assignedgod@users.noreply.github.com>
2025-08-20 13:55:24 -0400

    Revert "Attempt at exfiltrator bounty pad"
    This reverts commit 1da70a6d7b0c168f431d42cef27811caa4239f29.

--- Content.Server/_AS/SectorServices/StationSectorServiceHostComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Content.Server._AS.SectorServices;
-
-/// <summary>
-/// A station with this component will host all sector-wide services.
-/// </summary>
-[RegisterComponent]
-public sealed partial class StationSectorServiceHostComponent : Component
-{
-}
--- Resources/Locale/en-US/_AS/cargo/exfiltrator-bounties.ftl
+++
@@ -1,3 +0,0 @@
-﻿exfil-bounty-item-syndimask = Syndicate Gasmasks
-
-exfiltrator-bounty-description-generic = Missing assets detected. Recover if opportune.
