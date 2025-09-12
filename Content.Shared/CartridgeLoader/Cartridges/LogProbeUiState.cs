2ef81d1d955080c53fa99a3c6a4bb6d76c101725
AssignedGod <79281911+assignedgod@users.noreply.github.com>
2025-09-12 13:16:09 -0400

    Log probe old revert (whoops)

--- Content.Shared/CartridgeLoader/Cartridges/LogProbeUiState.cs
+++ Content.Shared/CartridgeLoader/Cartridges/LogProbeUiState.cs
@@ -7,25 +7,24 @@ namespace Content.Shared.CartridgeLoader.Cartridges;
 public sealed class LogProbeUiState : BoundUserInterfaceState
 {
     /// <summary>
-    /// The name of the scanned entity.
-    /// </summary>
-    public string EntityName;
-
-    /// <summary>
     /// The list of probed network devices
     /// </summary>
     public List<PulledAccessLog> PulledLogs;
 
-    public LogProbeUiState(List<PulledAccessLog> pulledLogs)
+    /// <summary>
+    /// DeltaV: The NanoChat data if a card was scanned, null otherwise
+    /// </summary>
+    public NanoChatData? NanoChatData { get; }
+
+    public LogProbeUiState(List<PulledAccessLog> pulledLogs, NanoChatData? nanoChatData = null) // DeltaV - NanoChat support
     {
-        EntityName = entityName;
         PulledLogs = pulledLogs;
         NanoChatData = nanoChatData; // DeltaV
     }
 }
 
 [Serializable, NetSerializable, DataRecord]
-public sealed partial class PulledAccessLog
+public sealed class PulledAccessLog
 {
     public readonly TimeSpan Time;
     public readonly string Accessor;
